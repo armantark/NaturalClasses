ecec90a9294379c6060af3994901fc40551015f3
armantark <armantarkhanian@berkeley.edu>
2018-09-28 22:37:11 -0700

    added in all sounds, need to actually put in the qualities tho

--- letters.py
+++ letters.py
@@ -3,18 +3,425 @@ from classes import classes
 letters = {
     "p": {
         classes.CONS,
+        classes.ORAL,
         classes.STOP,
         classes.BILAB,
         classes.LABIAL,
         classes.VOICELESS,
-        classes.ANTERIOR
+        classes.ANTERIOR,
+        classes.TENUIS,
+        classes.PLOSIVE
         },
     "b": {
         classes.CONS,
+        classes.ORAL,
         classes.STOP,
         classes.BILAB,
         classes.LABIAL,
         classes.VOICED,
-        classes.ANTERIOR
+        classes.ANTERIOR,
+        classes.PLOSIVE
+    },
+    "pʰ": {
+        classes.CONS,
+        classes.ORAL,
+        classes.STOP,
+        classes.BILAB,
+        classes.LABIAL,
+        classes.VOICELESS,
+        classes.ANTERIOR,
+        classes.ASPIRATED,
+        classes.PLOSIVE
+    },
+    "bʰ": {
+        classes.CONS,
+        classes.ORAL,
+        classes.STOP,
+        classes.BILAB,
+        classes.LABIAL,
+        classes.VOICED,
+        classes.ANTERIOR,
+        classes.ASPIRATED,
+        classes.PLOSIVE
+    },
+    "p'": {
+        classes.CONS,
+        classes.ORAL,
+        classes.BILAB,
+        classes.LABIAL,
+        classes.VOICELESS,
+        classes.ANTERIOR,
+        classes.EJECT,
+        classes.NONPULM,
+        classes.PLOSIVE
+    },
+    "m": {
+        classes.CONS,
+        classes.NASAL,
+        classes.BILABIAL,
+        classes.LABIAL,
+        classes.VOICED,
+        classes.ANTERIOR,
+        classes.STOP
+    },
+    "ʙ": {
+        classes.CONS,
+        classes.TRILL,
+        classes.BILABIAL,
+        classes.LABIAL,
+        classes.VOICED,
+        classes.ANTERIOR,
+        classes.VIBRANT
+    },
+    "ɸ": {
+
+    },
+    "β": {
+
+    },
+    "w": {
+
+    },
+    "ʍ": {
+
+    },
+    "ɱ": {
+
+    },
+    "ⱱ": {
+
+    },
+    "f": {
+
+    },
+    "v": {
+
+    },
+    "ʋ": {
+
+    },
+    "t": {
+
+    },
+    "d": {
+
+    },
+    "t'": {
+
+    },
+    "tʰ": {
+
+    },
+    "dʰ": {
+
+    },
+    "n": {
+
+    },
+    "r": {
+
+    },
+    "ɾ": {
+
+    },
+    "θ": {
+
+    },
+    "ð": {
+
+    },
+    "s": {
+
+    },
+    "z": {
+
+    },
+    "∫": {
+
+    },
+    "ʒ": {
+
+    },
+    "ɬ": {
+
+    },
+    "ɮ": {
+
+    },
+    "ɹ": {
+
+    },
+    "l": {
+
+    },
+    "ʈ": {
+
+    },
+    "ɖ": {
+
+    },
+    "ʈʰ": {
+
+    },
+    "ɖʰ": {
+
+    },
+    "ɳ": {
+
+    },
+    "ɽ": {
+
+    },
+    "ʂ": {
+
+    },
+    "ʐ": {
+
+    },
+    "ɻ": {
+
+    },
+    "ɭ": {
+
+    },
+    "c": {
+
+    },
+    "ɟ": {
+
+    },
+    "ɲ": {
+
+    },
+    "ç": {
+
+    },
+    "ʝ": {
+
+    },
+    "ɕ": {
+
+    },
+    "ʑ": {
+
+    },
+    "j": {
+
+    },
+    "ʎ": {
+
+    },
+    "k": {
+
+    },
+    "g": {
+
+    },
+    "k'": {
+
+    },
+    "kʰ": {
+
+    },
+    "gʰ": {
+
+    },
+    "ŋ": {
+
+    },
+    "x": {
+
+    },
+    "ɣ": {
+
+    },
+    "ɰ": {
+
+    },
+    "ʟ": {
+
+    },
+    "q": {
+
+    },
+    "ɢ": {
+
+    },
+    "ɴ": {
+
+    },
+    "ʀ": {
+
+    },
+    "χ": {
+
+    },
+    "ʁ": {
+
+    },
+    "ħ": {
+
+    },
+    "ʕ": {
+
+    },
+    "ʔ": {
+
+    },
+    "h": {
+
+    },
+    "ɦ": {
+
+    },
+    "i": {
+
+    },
+    "y": {
+
+    },
+    "ɨ": {
+
+    },
+    "ʉ": {
+
+    },
+    "ɯ": {
+
+    },
+    "u": {
+
+    },
+    "ɪ": {
+
+    },
+    "ʏ": {
+
+    },
+    "ʊ": {
+
+    },
+    "e": {
+
+    },
+    "ø": {
+
+    },
+    "ɘ": {
+
+    },
+    "ɵ": {
+
+    },
+    "ɤ": {
+
+    },
+    "o": {
+
+    },
+    "ə": {
+
+    },
+    "ɛ": {
+
+    },
+    "œ": {
+
+    },
+    "ɜ": {
+
+    },
+    "ɞ": {
+
+    },
+    "ʌ": {
+
+    },
+    "ɔ": {
+
+    },
+    "æ": {
+
+    },
+    "a": {
+
+    },
+    "ɶ": {
+
+    },
+    "ɑ": {
+
+    },
+    "ɒ": {
+
+    },
+    "ɐ": {
+
+    },
+    "ʘ": {
+
+    },
+    "ǀ": {
+
+    },
+    "ǃ": {
+
+    },
+    "ǂ": {
+
+    },
+    "ǁ": {
+
+    },
+    "ɓ": {
+
+    },
+    "ɗ": {
+
+    },
+    "ʄ": {
+
+    },
+    "ɠ": {
+
+    },
+    "ʛ": {
+
+    },
+    "ɥ": {
+
+    },
+    "ʜ": {
+
+    },
+    "ʢ": {
+
+    },
+    "ʡ": {
+
+    },
+    "ɺ": {
+
+    },
+    "ɧ": {
+
+    },
+    "ts": {
+
+    },
+    "dz": {
+
+    },
+    "t∫": {
+
+    },
+    "dʒ": {
+
+    },
+    "pf": {
+
+    },
+    "bv": {
+
     }
 }
